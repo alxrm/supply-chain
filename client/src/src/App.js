596d6018e54b5b72930d9a0fbd12b7c6d07bdab7
Alex <rupta3@gmail.com>
2018-01-03 00:26:20 +0300

    Added header

--- client/src/src/App.js
+++ client/src/src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import {Route, Redirect, Switch, BrowserRouter} from 'react-router-dom';
 import 'normalize.css';
 import 'flexboxgrid2';
+import styled from 'styled-components';
 import './App.css';
 
-export default () => (
+import Header from './Header';
+
+const Content = styled.div`
+  padding-top: 56px;
+`;
+
+export default ({ isAuthorized = true }) => (
   <BrowserRouter>
     <div className="App">
-      <Switch>
-        <Route path="/" component={() => (<div>Main</div>)} exact />
-        <Route path="/items/:uid" component={(props) => (<div>Item {props.match.params.uid}</div>)} />
-        <Route path="/login" component={() => (<div>Login</div>)} />
-        <Redirect from="**" to="/login" />
-      </Switch>
+      <Header />
+      <Content>
+        <Switch>
+          <Route path="/" component={() => (isAuthorized ? <div>Main</div> : <div>Login</div>)} exact />
+          <Route path="/items/:uid" component={(props) => (<div>Item {props.match.params.uid}</div>)} />
+          <Redirect from="**" to="/" />
+        </Switch>
+      </Content>
     </div>
   </BrowserRouter>
 );
