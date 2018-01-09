8fb044143b62b826fc759054455b040ff70c7457
Alex <rupta3@gmail.com>
2018-01-09 15:24:18 +0300

    Added

--- git-sync-commit.sh
+++ git-sync-commit.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+    echo 'Usage: ./git-sync-commit.sh <Commit message text here>';
+    exit 1;
+fi
+
 git commit . -m "$1";
 git checkout bitbucket;
 git merge master --allow-unrelated-histories;
