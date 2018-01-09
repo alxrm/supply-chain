0ad096da720a8051680d219a1e2d96fa7be57237
Alex <rupta3@gmail.com>
2018-01-09 15:09:18 +0300

    Added commit functionality to the git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git commit . -m $1;
 git checkout bitbucket;
 git merge master --allow-unrelated-histories;
 git push exonum-blockchain bitbucket;
+git checkout master;
+git push origin/master;
