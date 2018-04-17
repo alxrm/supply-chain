61b9c691f14a23b181f15692dbf5d32247a9702e
Alex <rupta3@gmail.com>
2018-04-17 22:57:00 +0300

    Added stub utils for product list

---
+++ client/src/utils/ProductUtils.js
@@ -0,0 +1,15 @@
+export const ProductUtils = {
+
+  /**
+   *
+   * @param productMap
+   * @returns {{
+   *  [groupdId]: [{ product1 }, { product2 }]
+   * }}
+   */
+  splitProductsByGroups(productMap = {}) {
+    return {};
+  }
+};
+
+export default ProductUtils;
